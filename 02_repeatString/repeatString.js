f3a28101087836fadc458a02afd1b531c12d5fa2
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-05-22 08:18:39 -0400

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  for (let i = 0; i < num; i++){
+    return string.repeat(3);
+  }
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
